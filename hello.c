fbc8a0750f7657b12f1f0e41092266a97f0db22d
rezasharifi82 <52376714+rezasharifi82@users.noreply.github.com>
2022-01-19 18:52:35 +0330

    in game menu fix

--- hello.c
+++ hello.c
@@ -575,8 +575,8 @@ NAMI:
                         int i = 198;
                         int j = 231;
                         int k = 7;
-                        int ho1, ho2,step;
-                        step=110;
+                        int ho1, ho2, step;
+                        step = 110;
                         ho1 = 250; ////x
                         ho2 = 200; ///y
                         int m, n, o;
@@ -628,7 +628,7 @@ NAMI:
                             x[2][2] = k;
                             al_draw_text(font, al_map_rgb(x[0][0], x[0][1], x[0][2]), ho1, ho2, 0, "Resume");
                             al_draw_text(font, al_map_rgb(x[1][0], x[1][1], x[1][2]), ho1, ho2 + step, 0, "Main Menu");
-                            al_draw_text(font, al_map_rgb(x[2][0], x[2][1], x[2][2]), ho1, ho2 + (2*step), 0, "Quit");
+                            al_draw_text(font, al_map_rgb(x[2][0], x[2][1], x[2][2]), ho1, ho2 + (2 * step), 0, "Quit");
                         }
                         if (vallahala == 2)
                         {
@@ -643,7 +643,7 @@ NAMI:
                             x[2][2] = k;
                             al_draw_text(font, al_map_rgb(x[0][0], x[0][1], x[0][2]), ho1, ho2, 0, "Resume");
                             al_draw_text(font, al_map_rgb(x[1][0], x[1][1], x[1][2]), ho1, ho2 + step, 0, "Main Menu");
-                            al_draw_text(font, al_map_rgb(x[2][0], x[2][1], x[2][2]), ho1, ho2 + (2*step), 0, "Quit");
+                            al_draw_text(font, al_map_rgb(x[2][0], x[2][1], x[2][2]), ho1, ho2 + (2 * step), 0, "Quit");
                         }
                         if (vallahala == 3)
                         {
@@ -658,12 +658,17 @@ NAMI:
                             x[2][2] = o;
                             al_draw_text(font, al_map_rgb(x[0][0], x[0][1], x[0][2]), ho1, ho2, 0, "Resume");
                             al_draw_text(font, al_map_rgb(x[1][0], x[1][1], x[1][2]), ho1, ho2 + step, 0, "Main Menu");
-                            al_draw_text(font, al_map_rgb(x[2][0], x[2][1], x[2][2]), ho1, ho2 + (2*step), 0, "Quit");
+                            al_draw_text(font, al_map_rgb(x[2][0], x[2][1], x[2][2]), ho1, ho2 + (2 * step), 0, "Quit");
                         }
                         al_flip_display();
                     } //while
-                        
-                    if (vallahala == 2)
+
+                    if (vallahala == 1)
+                    {
+                        key[ALLEGRO_KEY_ESCAPE]=0;
+                    }
+
+                    else if (vallahala == 2)
                     {
                         goto MAIN;
                     }
@@ -845,52 +850,128 @@ NAMI:
                         nobat = 2;
                     }
                 }
-
                 if (key[ALLEGRO_KEY_ESCAPE])
                 {
-                    al_get_keyboard_state(&ks);
+
                     int vallahala = 1;
+                    int x[3][3];
+                    x[0][0] = 107; //red
+                    x[0][1] = 5;
+                    x[0][2] = 5;
+                    //yellow 198, 231, 7
+                    x[1][0] = 198;
+                    x[1][1] = 231;
+                    x[1][2] = 7;
+                    x[2][0] = 198;
+                    x[2][1] = 231;
+                    x[2][2] = 7;
                     while (!al_key_down(&ks, ALLEGRO_KEY_SPACE))
                     {
 
-                        int i, j, k, x, y, z, m, n, o;
-                        i = 107; //red
-                        j = 5;
-                        k = 5;
-                        //blue
-                        x = 3;
-                        y = 1;
-                        z = 31;
-                        al_clear_to_color(al_map_rgb(3, 1, 31));
-                        al_draw_bitmap(letter, 150, 150, 0);
-                        al_draw_text(font1, al_map_rgb(x, y, z), 200, 200, 0, "Resume");
-                        al_draw_text(font1, al_map_rgb(x, y, z), 200, 250, 0, "Main Menu");
-                        al_draw_text(font1, al_map_rgb(x, y, z), 200, 300, 0, "Quit");
+                        int i = 198;
+                        int j = 231;
+                        int k = 7;
+                        int ho1, ho2, step;
+                        step = 110;
+                        ho1 = 250; ////x
+                        ho2 = 200; ///y
+                        int m, n, o;
+                        m = 107;
+                        n = 5;
+                        o = 5;
+                        al_wait_for_event(queue, &event);
                         al_get_keyboard_state(&ks);
-                        if (al_key_down(&ks, ALLEGRO_KEY_1))
+                        switch (event.type)
                         {
-                            al_draw_text(font1, al_map_rgb(i, j, k), 200, 200, 0, "Resume");
-                            vallahala = 1;
+                        case ALLEGRO_EVENT_KEY_DOWN:
+                            if (event.keyboard.keycode == ALLEGRO_KEY_DOWN)
+                            {
+
+                                if (vallahala == 1)
+                                    vallahala += 1;
+                                else if (vallahala == 2)
+                                    vallahala += 1;
+                                else
+                                    vallahala = 1;
+                            }
+                            else if (event.keyboard.keycode == ALLEGRO_KEY_UP)
+                            {
+                                if (vallahala == 1)
+                                    vallahala = 3;
+                                else if (vallahala == 2)
+                                    vallahala = 1;
+                                else
+                                    vallahala = 2;
+                            }
+                            break;
+                        case ALLEGRO_EVENT_DISPLAY_CLOSE:
+                            fti = -1;
+                            goto QUIT;
+
+                            break;
+                        } //switch
+                        al_clear_to_color(al_map_rgb(3, 1, 22));
+                        if (vallahala == 1)
+                        {
+                            x[1][0] = i;
+                            x[0][0] = m;
+                            x[1][1] = j;
+                            x[0][1] = n;
+                            x[1][2] = k;
+                            x[0][2] = o;
+                            x[2][0] = i;
+                            x[2][1] = j;
+                            x[2][2] = k;
+                            al_draw_text(font, al_map_rgb(x[0][0], x[0][1], x[0][2]), ho1, ho2, 0, "Resume");
+                            al_draw_text(font, al_map_rgb(x[1][0], x[1][1], x[1][2]), ho1, ho2 + step, 0, "Main Menu");
+                            al_draw_text(font, al_map_rgb(x[2][0], x[2][1], x[2][2]), ho1, ho2 + (2 * step), 0, "Quit");
                         }
-                        else if (al_key_down(&ks, ALLEGRO_KEY_2))
+                        if (vallahala == 2)
                         {
-                            al_draw_text(font1, al_map_rgb(i, j, k), 200, 250, 0, "Main Menu");
-                            vallahala = 2;
+                            x[1][0] = m;
+                            x[0][0] = i;
+                            x[1][1] = n;
+                            x[0][1] = j;
+                            x[1][2] = o;
+                            x[0][2] = k;
+                            x[2][0] = i;
+                            x[2][1] = j;
+                            x[2][2] = k;
+                            al_draw_text(font, al_map_rgb(x[0][0], x[0][1], x[0][2]), ho1, ho2, 0, "Resume");
+                            al_draw_text(font, al_map_rgb(x[1][0], x[1][1], x[1][2]), ho1, ho2 + step, 0, "Main Menu");
+                            al_draw_text(font, al_map_rgb(x[2][0], x[2][1], x[2][2]), ho1, ho2 + (2 * step), 0, "Quit");
                         }
-                        else if (al_key_down(&ks, ALLEGRO_KEY_3))
+                        if (vallahala == 3)
                         {
-                            al_draw_text(font1, al_map_rgb(i, j, k), 200, 300, 0, "Quit");
-                            vallahala = 3;
+                            x[1][0] = i;
+                            x[0][0] = i;
+                            x[1][1] = j;
+                            x[0][1] = j;
+                            x[1][2] = k;
+                            x[0][2] = k;
+                            x[2][0] = m;
+                            x[2][1] = n;
+                            x[2][2] = o;
+                            al_draw_text(font, al_map_rgb(x[0][0], x[0][1], x[0][2]), ho1, ho2, 0, "Resume");
+                            al_draw_text(font, al_map_rgb(x[1][0], x[1][1], x[1][2]), ho1, ho2 + step, 0, "Main Menu");
+                            al_draw_text(font, al_map_rgb(x[2][0], x[2][1], x[2][2]), ho1, ho2 + (2 * step), 0, "Quit");
                         }
                         al_flip_display();
                     } //while
-                    if (vallahala == 2)
+
+                    if (vallahala == 1)
+                    {
+                        key[ALLEGRO_KEY_ESCAPE]=0;
+                    }
+
+                    else if (vallahala == 2)
                     {
                         goto MAIN;
                     }
                     else if (vallahala == 3)
-                    {
 
+                    {
+                        fti = -1;
                         goto QUIT;
                     }
                 }
